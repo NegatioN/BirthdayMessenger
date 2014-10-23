9926bc3af1cc56f5a4525bc93481ec7bfebe9771
Joakim Rishaug <Joakimrishaug@gmail.com>
2014-10-23 22:57:04 +0200

    does not start service on boot if user-setting is correct.

--- app/src/main/java/main/soakim/no/birthdaymessenger/smsservice/SmsBroadcastReciever.java
+++ app/src/main/java/main/soakim/no/birthdaymessenger/smsservice/SmsBroadcastReciever.java
@@ -3,6 +3,9 @@ package main.soakim.no.birthdaymessenger.smsservice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 /**
  * Created by NegatioN on 19.10.2014.
@@ -10,9 +13,15 @@ import android.content.Intent;
 public class SmsBroadcastReciever extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        //Intent i = new Intent(context, SmsService.class);
+
         // real
-        Intent i = new Intent(context, PeriodicService.class);
-        context.startService(i);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if(preferences.getBoolean("checkbox_preference", false)) {
+            Intent i = new Intent(context, PeriodicService.class);
+            context.startService(i);
+        }
+        else{
+            Log.d("Broadcastrec", "Service not booting");
+        }
     }
 }
